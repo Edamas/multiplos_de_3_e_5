834e33cae242b6121deb945f095403bc76df2781
Elysio Neto <49333427+Edamas@users.noreply.github.com>
2021-01-30 15:16:38 -0300

    Update multiplos3_5_em_varias_linhas.py

--- multiplos3_5_em_varias_linhas.py
+++ multiplos3_5_em_varias_linhas.py
@@ -2,11 +2,11 @@ numero_inicial = 1
 numero_final   = 100
 
 dic_resultados = {
-					(False, False):'',
-					(True , False):'IT',
-					(False, True ):'BHUT',
-					(True , True ):'BHUT TI'
-				}
+		(False, False):'',
+		(True , False):'BHUT',
+		(False, True ):'IT',
+		(True , True ):'BHUT TI'
+		}
 
 for numero in range(numero_inicial, numero_final + 1):
 	
@@ -14,14 +14,14 @@ for numero in range(numero_inicial, numero_final + 1):
 	eh_multiplo_de_5 = numero % 5 == 0
 	
 	chave = (
-			eh_multiplo_de_3,
-			eh_multiplo_de_5
-			)
+		eh_multiplo_de_3,
+		eh_multiplo_de_5
+		)
 	
 	frase_de_resultado = dic_resultados[chave]
 	
 	print(
-			numero,
-			frase_de_resultado,
-			end='\t'
+		numero,
+		frase_de_resultado,
+		end='\t'
 		)
